7a45c189284c9c7c0941034a2566c5e171e82455
Arbaz Khan <146950002+arbazzkhann@users.noreply.github.com>
2024-10-14 19:17:15 +0530

    creating routes (about, contact) within nextJS

---
+++ NextJS-with-React/1.first-nextjs-app/app/contact/page.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const page = () => {
+  return (
+    <div className="">
+      <p className="text-3xl font-bold m-5">Hello this is Contact Page.</p>
+    </div>
+  );
+};
+
+export default page;
