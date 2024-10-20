fdfbe0d6bcff89745837a0fa60127fd60bd19fe1
Arbaz Khan <146950002+arbazzkhann@users.noreply.github.com>
2024-10-20 10:30:00 +0530

    use client and server component in NextJS

---
+++ NextJS-with-React/2.setting-up-nextjs/app/page.js
@@ -0,0 +1,13 @@
+import Navbar from "./Components/Navbar";
+
+export default function Home() {
+
+  return (
+    <>
+      <div>
+        <Navbar />
+        <h1>Hello World, This is home page.</h1>
+      </div>
+    </>
+  );
+}
