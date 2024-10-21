178615dfda386065c626921ebccf3150506b6f5e
Arbaz Khan <146950002+arbazzkhann@users.noreply.github.com>
2024-10-21 12:43:43 +0530

    using Link tag in  Navbar

--- NextJS-with-React/3.scipt-link-image-components/app/Components/Navbar.js
+++ NextJS-with-React/3.scipt-link-image-components/app/Components/Navbar.js
@@ -9,7 +9,7 @@ const Navbar = () => {
       <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
         <Link href="/" class="flex items-center space-x-3 rtl:space-x-reverse">
           <img src="https://flowbite.com/docs/images/logo.svg" class="h-8" alt="Flowbite Logo"/>
-          <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white"> Demo Logo </span>
+          <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white"> ARBAZ KHAN </span>
         </Link>
         <button
           data-collapse-toggle="navbar-dropdown"
@@ -39,7 +39,7 @@ const Navbar = () => {
           <ul class="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
             <li>
               <Link
-                href="#"
+                href="/"
                 class="block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500 dark:bg-blue-600 md:dark:bg-transparent"
                 aria-current="page"
               >
@@ -114,26 +114,19 @@ const Navbar = () => {
             </li>
             <li>
               <Link
-                href=""
-                class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
-              >
-                Services
-              </Link>
-            </li>
-            <li>
-              <Link
-                href=""
-                class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
-              >
-                Pricing
-              </Link>
-            </li>
-            <li>
-              <Link
-                href=""
+                href="/contact  "
                 class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
               >
                 Contact
+              </Link>
+            </li>
+
+            <li>
+              <Link
+                href="/about"
+                class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+              >
+                About
               </Link>
             </li>
           </ul>
--- NextJS-with-React/3.scipt-link-image-components/app/about/page.js
+++ NextJS-with-React/3.scipt-link-image-components/app/about/page.js
@@ -7,3 +7,8 @@ const about = () => {
 }
 
 export default about
+
+export const metadata = {
+  title: "About Demo - Connect with World",
+  description: "This is about Demo and we can connect easily with the world using Demo"
+};
