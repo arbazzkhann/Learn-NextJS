b30365b5d4d2d2519301d8099a47cf4bd3f1c867
Arbaz Khan <146950002+arbazzkhann@users.noreply.github.com>
2024-10-21 18:03:38 +0530

    Image component in NextJS

--- NextJS-with-React/3.scipt-link-image-components/app/page.js
+++ NextJS-with-React/3.scipt-link-image-components/app/page.js
@@ -1,7 +1,18 @@
 import Image from "next/image";
 
+
 export default function Home() {
   return (
-    <div></div>
+    <div>
+      <div className="container">
+      {/* <img className='mx-auto h-60 my-4 rounded-xl' src='https://dfstudio-d420.kxcdn.com/wordpress/wp-content/uploads/2019/06/digital_camera_photo-1080x675.jpg'/> */}
+        <Image
+          src="https://dfstudio-d420.kxcdn.com/wordpress/wp-content/uploads/2019/06/digital_camera_photo-1080x675.jpg"
+          alt="Picture of the author"
+          width={500}
+          height={500}
+        />
+      </div>
+    </div>
   );
 }
