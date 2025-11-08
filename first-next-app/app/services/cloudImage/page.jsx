85836cfdc031d4623eea16ef370446c37c88825d
Arbaz Khan <arbazkha78az@gmail.com>
2025-11-08 21:04:35 +0530

    nested routing in nextjs

---
+++ first-next-app/app/services/cloudImage/page.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>This is a cloud image service.</div>
+  )
+}
+
+export default page
---
+++ first-next-app/app/services/db/page.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>This is a database service.</div>
+  )
+}
+
+export default page
